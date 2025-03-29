7c8efbb41ac0a49465f91ba58a69b02da255fad4
Fatima Ali <fatimaaliforwork@gmail.com>
2025-03-29 17:05:31 +0500

    updates cors

--- portalized/settings.py
+++ portalized/settings.py
@@ -136,6 +136,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
    "http://localhost:5173",
+   "https://portalized-admin-panel-smoky.vercel.app",
+   "https://portalized-frontend.vercel.app"
    
 ]
 
