bd0ad005dae633baa5210e093f132e3c6bcba7c8
Fatima Ali <fatimaaliforwork@gmail.com>
2025-04-14 12:13:04 +0500

    updates views

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,14 +2,13 @@ from rest_framework import serializers
 from .models import Post, Like, Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    user_email = serializers.EmailField(source='user.email', read_only=True)
     user_first_name = serializers.CharField(source='user.first_name', read_only=True)
     user_last_name = serializers.CharField(source='user.last_name', read_only=True)
     user_profile_picture = serializers.CharField(source='user.profile_picture', read_only=True)
 
     class Meta:
         model = Comment
-        fields = ['id', 'user', 'user_email', 'post', 'content', 'created_at',  'user_first_name', 'user_last_name',
+        fields = ['id', 'user',  'post', 'content', 'created_at',  'user_first_name', 'user_last_name',
             'user_profile_picture']
         read_only_fields = ['id', 'created_at']
 
@@ -22,7 +21,6 @@ class LikeSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'created_at']
 
 class PostSerializer(serializers.ModelSerializer):
-    user_email = serializers.EmailField(source='user.email', read_only=True)
     likes_count = serializers.IntegerField(source='likes.count', read_only=True)
     comments_count = serializers.IntegerField(source='comments.count', read_only=True)
     is_liked_by_user = serializers.SerializerMethodField()
@@ -33,7 +31,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'user', 'user_email', 'caption', 'media_urls',
+            'id', 'user',  'caption', 'media_urls',
             'created_at', 'likes_count', 'comments_count', 'is_liked_by_user', 
             'post_type', 'location', 'music', 'privacy', 'comments','user_first_name', 'user_last_name',
             'user_profile_picture'
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,14 @@
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets, permissions
 from .models import Post, Like, Comment
 from .serializers import PostSerializer, LikeSerializer, CommentSerializer
 
+class CommentPagination(PageNumberPagination):
+    page_size = 10  # Default comments per page
+    page_size_query_param = 'page_size'
+    max_page_size = 50
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
@@ -12,32 +18,32 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    # Override the list method to add "is_liked" field in response
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
-
-        # Add "is_liked" to each post
         posts_with_likes = []
         for post in serializer.data:
             post['is_liked'] = Post.objects.get(id=post['id']).is_liked_by_user(user=request.user)
             posts_with_likes.append(post)
-
         return Response(posts_with_likes)
 
-    # Override the retrieve method to include comments for a single post
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
-        
-        # Set context variable to include comments for single post
         serializer.context['is_single_post'] = True
         return Response(serializer.data)
 
-    # Don't include comments in the response for the list of posts
     def get_queryset(self):
         return Post.objects.prefetch_related('likes').all()
 
+    @action(detail=True, methods=['get'], url_path='comments')
+    def paginated_comments(self, request, pk=None):
+        post = self.get_object()
+        comments = Comment.objects.filter(post=post).order_by('-created_at')
+        paginator = CommentPagination()
+        result_page = paginator.paginate_queryset(comments, request)
+        serializer = CommentSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
 class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
