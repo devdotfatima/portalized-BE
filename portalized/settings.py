f7b3e600b831b52e006345ee98286786f2d40d00
Fatima Ali <fatimaaliforwork@gmail.com>
2025-04-15 11:25:37 +0500

    fixes

--- portalized/settings.py
+++ portalized/settings.py
@@ -110,9 +110,7 @@ REST_FRAMEWORK = {
       "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
      "PAGE_SIZE": 10,  # Show 10 products per page
       'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-       'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    
-    
+
 }
 
 
