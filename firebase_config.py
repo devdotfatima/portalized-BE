5cd87cebe577371179f3e255e0e8b1347e747368
Fatima Ali <fatimaaliforwork@gmail.com>
2025-04-29 16:20:32 +0500

    updates firebase config

--- firebase_config.py
+++ firebase_config.py
@@ -1,18 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
-
-# # Path to the downloaded service account key file
-# cred = credentials.Certificate('portalized-4c162-firebase-adminsdk-fbsvc-fcfff0a0d7.json')
-
-# # Initialize Firebase app
-# firebase_admin.initialize_app(cred)
-
-
-
-# import firebase_admin
-# from firebase_admin import credentials
 import json
-from django.conf import settings  # Import settings to access FIREBASE_CREDENTIALS
+from django.conf import settings
 
 # Get the Firebase credentials from the environment variable
 firebase_credentials_str = settings.FIREBASE_CREDENTIALS
@@ -21,9 +10,10 @@ if firebase_credentials_str:
     # Parse the Firebase credentials JSON string into a dictionary
     firebase_credentials_dict = json.loads(firebase_credentials_str)
     
-    # Initialize Firebase with the credentials
-    cred = credentials.Certificate(firebase_credentials_dict)
-    firebase_admin.initialize_app(cred)
-    print("Firebase app initialized successfully!")
+    # Check if Firebase is already initialized
+    if not firebase_admin._apps:
+        cred = credentials.Certificate(firebase_credentials_dict)
+        firebase_admin.initialize_app(cred)
+        print("✅ Firebase app initialized successfully!")
 else:
     print("❌ Firebase credentials not found in environment variables!")
